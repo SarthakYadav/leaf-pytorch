0059b23a4d53c75bb6b020771aa61a060a88136d
Sarthak Yadav <sarthak.yadav3@gmail.com>
2021-12-14 00:17:56 +0000

    added final voxceleb cfgs

--- voxceleb_run.sh
+++ voxceleb_run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-python train_xla.py --cfg_file ./cfgs/voxceleb/efficientnet-b0-leaf-default.cfg -e ~/leaf_experiments/voxceleb/efficientnet-b0_default_leaf_bs1x256_adam_wd_1e-4_rs8881_legacycomplex --epochs 50 --num_workers 4 --log_steps 50 --random_seed 8881 --wandb_group voxceleb
-python train_xla.py --cfg_file ./cfgs/voxceleb/efficientnet-b0-leaf-default.cfg -e ~/leaf_experiments/voxceleb/efficientnet-b0_default_leaf_bs1x256_adam_wd_1e-4_rs8882_legacycomplex --epochs 50 --num_workers 4 --log_steps 50 --random_seed 8882 --wandb_group voxceleb
-python train_xla.py --cfg_file ./cfgs/voxceleb/efficientnet-b0-leaf-default.cfg -e ~/leaf_experiments/voxceleb/efficientnet-b0_default_leaf_bs1x256_adam_wd_1e-4_rs8883_legacycomplex --epochs 50 --num_workers 4 --log_steps 50 --random_seed 8883 --wandb_group voxceleb
+# python train_xla.py --cfg_file ./cfgs/voxceleb/efficientnet-b0-leaf-default.cfg -e ~/leaf_experiments/voxceleb/efficientnet-b0_default_leaf_bs1x256_adam_warmupcosine_wd_1e-4_rs8881_legacycomplex --epochs 100 --num_workers 4 --log_steps 50 --random_seed 8881 --wandb_group voxceleb
+# python train_xla.py --cfg_file ./cfgs/voxceleb/efficientnet-b0-leaf-default.cfg -e ~/leaf_experiments/voxceleb/efficientnet-b0_default_leaf_bs1x256_adam_warmupcosine_wd_1e-4_rs8882_legacycomplex --epochs 100 --num_workers 4 --log_steps 50 --random_seed 8882 --wandb_group voxceleb
+# python train_xla.py --cfg_file ./cfgs/voxceleb/efficientnet-b0-leaf-default.cfg -e ~/leaf_experiments/voxceleb/efficientnet-b0_default_leaf_bs1x256_adam_warmupcosine_wd_1e-4_rs8883_legacycomplex --epochs 100 --num_workers 4 --log_steps 50 --random_seed 8883 --wandb_group voxceleb
+
+python train_xla.py --cfg_file ./cfgs/voxceleb/resnet-18-leaf-default.cfg -e ~/leaf_experiments/voxceleb/resnet-18_default_leaf_bs1x256_adam_warmupcosine_wd_1e-4_rs8881_legacycomplex --epochs 100 --num_workers 4 --log_steps 50 --random_seed 8881 --wandb_group voxceleb
+python train_xla.py --cfg_file ./cfgs/voxceleb/resnet-18-leaf-default.cfg -e ~/leaf_experiments/voxceleb/resnet-18_default_leaf_bs1x256_adam_warmupcosine_wd_1e-4_rs8882_legacycomplex --epochs 100 --num_workers 4 --log_steps 50 --random_seed 8882 --wandb_group voxceleb
+python train_xla.py --cfg_file ./cfgs/voxceleb/resnet-18-leaf-default.cfg -e ~/leaf_experiments/voxceleb/resnet-18_default_leaf_bs1x256_adam_warmupcosine_wd_1e-4_rs8883_legacycomplex --epochs 100 --num_workers 4 --log_steps 50 --random_seed 8883 --wandb_group voxceleb
